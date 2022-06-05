42b6afac9f6298a88c15e0f489eab3490f0933a0
stamovm <m.stamov@yahoo.com>
2022-06-04 21:09:56 -0700

    Initial commit.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">ICommerce</a>
+      </header>
+      <main>
+        <div className="container">
+          <h3>list of products</h3>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
