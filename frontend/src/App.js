69e64c11d17736b7404ff87c07fca3545fdddbfa
stamovm <m.stamov@yahoo.com>
2022-06-05 12:58:53 -0700

    Added products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
+import data from './data.js'
+
 function App() {
+  //50:16
+
   return (
     <div>
       <header className="App-header">
         <a href="/">ICommerce</a>
       </header>
       <main>
-        <div className="container">
-          <h3>list of products</h3>
+        <h2>List of products</h2>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
     </div>
